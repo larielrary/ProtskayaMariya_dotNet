73b2b3da46e25ca679c8c3777205b89952abf165
larielrary <44781126+larielrary@users.noreply.github.com>
2020-05-23 16:37:28 +0300

    Add business layer files

---
+++ ServiceStation/BusinessLayer/Models/CarTechnicalCondition.cs
@@ -0,0 +1,39 @@
+﻿using DataLayer.Entity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace BusinessLayer.Models.DTO
+{
+    [DataContract]
+    public class CarTechnicalCondition : IEntity
+    {
+        [Key]
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public DateTime Date { get; set; }
+        [DataMember]
+        public double Milleage { get; set; }
+        [DataMember]
+        public bool BreakSystem { get; set; }
+        [DataMember]
+        public bool Suspension { get; set; }
+        [DataMember]
+        public bool Wheels { get; set; }
+        [DataMember]
+        public bool Lighting { get; set; }
+        [DataMember]
+        public double CarbonDioxideContent { get; set; }
+        [DataMember]
+        public int InspectorId { get; set; }
+        [DataMember]
+        public int CarId { get; set; }
+        [DataMember]
+        public bool InspectionMark { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} {Date} {Milleage} {BreakSystem} {Suspension} {Wheels} {Lighting} {CarbonDioxideContent} {InspectorId} {CarId} {InspectionMark}";
+        }
+    }
+}
---
+++ ServiceStation/BusinessLayer/ServiceStationService/OwnerService.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using BusinessLayer.Models.DTO;
+using DataLayer.Models;
+using DataLayer.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ServiceStationService
+{
+    public class OwnerService : IEntityService<Owner>
+    {
+        private readonly IRepository<OwnerDTO> _ownerRepository;
+        private readonly IMapper _mapper;
+        public OwnerService(IRepository<OwnerDTO> ownerRepository, IMapper mapper)
+        {
+            _ownerRepository = ownerRepository;
+            _mapper = mapper;
+        }
+        public async Task<Owner> GetItem(int id)
+        {
+            var owner = await _ownerRepository.GetById(id);
+
+            if (owner == null)
+            {
+                throw new ArgumentNullException(nameof(owner));
+            }
+
+            return new Owner
+            {
+                Id = owner.Id,
+                Firstname = owner.Firstname,
+                LastName = owner.LastName,
+                MiddleName = owner.MiddleName,
+                PhoneNum = owner.PhoneNum
+            };
+        }
+        public async Task<IEnumerable<Owner>> GetItems()
+        {
+            return _mapper.Map<IEnumerable<OwnerDTO>, List<Owner>>(await _ownerRepository.GetAll());
+        }
+
+        public async Task Create(Owner item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            else
+            {
+                await _ownerRepository.Add(_mapper.Map<OwnerDTO>(item));
+            }
+        }
+
+        public async Task Update(Owner item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            else
+            {
+                await _ownerRepository.Update(_mapper.Map<OwnerDTO>(item));
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            var item = GetItems().Result.Where(el => el.Id == id).ToList();
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            else
+            {
+                await _ownerRepository.Delete(_mapper.Map<OwnerDTO>(item[0]).Id);
+            }
+        }
+    }
+}

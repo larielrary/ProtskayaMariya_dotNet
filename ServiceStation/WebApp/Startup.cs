8f8554aeb59d5260d4b4f78f7b2e5045dd1d1b3c
larielrary <44781126+larielrary@users.noreply.github.com>
2020-05-26 14:39:04 +0300

    Fixed register bug

--- ServiceStation/WebApp/Startup.cs
+++ ServiceStation/WebApp/Startup.cs
@@ -29,8 +29,11 @@ namespace WebApp
             services.AddDbContext<ServiceStationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
 
-            services.AddIdentity<IdentityUser, IdentityRole>()
-               .AddEntityFrameworkStores<ServiceStationContext>();
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                        .AddEntityFrameworkStores<ServiceStationContext>();
+
+
+
             services.AddControllersWithViews();
             services.AddRazorPages();
 
@@ -59,7 +62,9 @@ namespace WebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
+
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
@@ -67,8 +72,8 @@ namespace WebApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }

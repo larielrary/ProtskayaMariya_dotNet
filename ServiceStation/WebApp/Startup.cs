6d7a2355d6cd4603096bfdc83c609b785efd4bf3
larielrary <44781126+larielrary@users.noreply.github.com>
2020-05-30 18:25:09 +0300

    Update Startup.cs

--- ServiceStation/WebApp/Startup.cs
+++ ServiceStation/WebApp/Startup.cs
@@ -49,7 +49,6 @@ namespace WebApp
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
